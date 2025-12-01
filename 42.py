0d58aa9dbeaafce18f837f5076ba9c57027cf452
samarawwleticia <samaradddsantos@gmail.com>
2025-12-01 20:28:29 -0300

    correção

--- 42.py
+++ 42.py
@@ -3,10 +3,7 @@
 
 class Solution:
     def trap(self, height: list[int]) -> int:
-        """
-        Calcula a quantidade de água da chuva que pode ser retida entre as barras 
-        usando o método otimizado de Two Pointers.
-        """
+
         if not height:
             return 0
 
@@ -18,7 +15,6 @@ class Solution:
 
         while left < right:
             if height[left] < height[right]:
-                # O limite é o lado esquerdo
                 if height[left] >= max_left:
                     max_left = height[left]
                 else:
@@ -26,7 +22,6 @@ class Solution:
                 
                 left += 1
             else:
-                # O limite é o lado direito
                 if height[right] >= max_right:
                     max_right = height[right]
                 else:
