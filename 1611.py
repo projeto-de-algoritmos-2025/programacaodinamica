f92e97215ab4a9e377a2739337462eef60041dad
samarawwleticia <samaradddsantos@gmail.com>
2025-12-01 19:27:09 -0300

    solucao 2

--- 1611.py
+++ 1611.py
@@ -7,11 +7,21 @@ class Solution:
 
         max_bits = n.bit_length()
 
-        # dp[k] = Mín. operações para transformar 2^k em 0. (2^(k+1) - 1)
         dp = [0] * (max_bits + 1)
         dp[0] = 1 
     
         for k in range(1, max_bits):
             dp[k] = 2 * dp[k-1] + 1
+
+        def solve(current_n):
+            if current_n == 0:
+                return 0
+        
+            k = current_n.bit_length() - 1
+            power_of_2 = 1 << k
+        
+            remainder = current_n ^ power_of_2 
+        
+            return dp[k] - solve(remainder)
     
         return solve(n)
