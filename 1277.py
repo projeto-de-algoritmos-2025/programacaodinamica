c4f2c65c65b42ac885dd1c05f6f84b64584369b6
samarawwleticia <samaradddsantos@gmail.com>
2025-12-01 18:13:56 -0300

    relacao de recorrencia

--- 1277.py
+++ 1277.py
@@ -1,1 +1,29 @@
 #contar quantas submatrizes quadradas contêm apenas 1s
+
+class Solution:
+    def countSquares(self, matrix: list[list[int]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        total_squares = 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                
+                if matrix[r][c] == 1:
+                    
+                    if r == 0 or c == 0:
+                        pass
+                    
+                    else:
+                        matrix[r][c] = min(
+                            matrix[r - 1][c],      # superior
+                            matrix[r][c - 1],      # esquerdo
+                            matrix[r - 1][c - 1]   # diagonal
+                        ) + 1
+                
+                total_squares += matrix[r][c]
+
+        return total_squares
